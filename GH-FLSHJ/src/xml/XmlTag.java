626354f0adc77e826ccac55369f89641922c8029
Anas Rchid <0x0584@protonmail.com>
2018-05-15 12:43:56 +0000

    create XmlScheme super class
    this class describes how an xml tag should be

---
+++ GH-FLSHJ/src/xml/XmlTag.java
@@ -0,0 +1,37 @@
+package xml;
+
+import java.util.Arrays;
+
+public class XmlTag {
+	private String XmlAttrs[], root;
+	public static short PARENT = 0;
+
+	public XmlTag(String root, String[] xmlAttrs) {
+		super( );
+		this.XmlAttrs = xmlAttrs;
+		this.root = root;
+	}
+
+	@Override
+	public String toString( ) {
+		return "XmlTag [XmlAttrs=" + Arrays.toString(XmlAttrs) + ", content="
+						+ root + "]";
+	}
+
+	public String[] getXmlAttrs( ) {
+		return XmlAttrs;
+	}
+
+	public void setXmlAttrs(String[] xmlAttrs) {
+		XmlAttrs = xmlAttrs;
+	}
+
+	public String getRoot( ) {
+		return root;
+	}
+
+	public void setRoot(String root) {
+		this.root = root;
+	}
+
+}
