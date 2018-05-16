79d5ec35f8b67fdc4a582b0ef3ea167cdcb9bf77
Anas Rchid <0x0584@protonmail.com>
2018-05-16 20:15:47 +0000

    update the Employee class

--- GH-FLSHJ/src/testing/Main.java
+++ GH-FLSHJ/src/testing/Main.java
@@ -1,15 +1,10 @@
 package testing;
 
-import java.util.ArrayList;
-
-import model.Diploma;
-import xml.XmlFile;
 
 public class Main {
 
 	public static void main(String[] args) {
-		XmlFile x = new XmlFile("data/hr.xml");
-		ArrayList<Diploma> tmp = x.getDiplomas( );
+
 		
 	}
 
