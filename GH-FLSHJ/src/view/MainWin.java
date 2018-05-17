f1e2cce4a884c51e5a7db17a8eada32a34565054
Anas Rchid <0x0584@protonmail.com>
2018-05-17 12:04:23 +0000

    switch label names to Arabic because it's lafac dial l2adab?

--- GH-FLSHJ/src/view/MainWin.java
+++ GH-FLSHJ/src/view/MainWin.java
@@ -29,6 +29,7 @@ import model.Employee.Cadre;
 import javax.swing.JSpinner;
 import java.awt.SystemColor;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.JScrollPane;
 
 public class MainWin {
 
@@ -99,50 +100,50 @@ public class MainWin {
 		panel.add(tabbedPane);
 
 		JPanel panel_1 = new JPanel( );
-		tabbedPane.addTab("Information Personnelle", null, panel_1, null);
+		tabbedPane.addTab("البطاقة الشخصية", null, panel_1, null);
 		panel_1.setLayout(null);
 
-		JLabel label = new JLabel("First name");
-		label.setBounds(25, 15, 84, 15);
+		JLabel label = new JLabel("الإسم الشخصي");
+		label.setBounds(25, 15, 98, 15);
 		panel_1.add(label);
 
-		JLabel label_1 = new JLabel("Last name");
-		label_1.setBounds(25, 45, 84, 15);
+		JLabel label_1 = new JLabel("الإسم العائلي");
+		label_1.setBounds(32, 45, 84, 15);
 		panel_1.add(label_1);
 
-		JLabel label_2 = new JLabel("Birth info");
-		label_2.setBounds(25, 75, 84, 15);
+		JLabel label_2 = new JLabel("تاريخ وَ مكان الإزدياد");
+		label_2.setBounds(12, 75, 124, 15);
 		panel_1.add(label_2);
 
-		JLabel label_3 = new JLabel("Married?");
-		label_3.setBounds(289, 22, 84, 15);
+		JLabel label_3 = new JLabel("متزوج؟");
+		label_3.setBounds(334, 22, 61, 15);
 		panel_1.add(label_3);
 
-		JLabel label_4 = new JLabel("Partner name");
-		label_4.setBounds(276, 59, 111, 15);
+		JLabel label_4 = new JLabel("إسم الزوج");
+		label_4.setBounds(327, 59, 75, 15);
 		panel_1.add(label_4);
 
-		JLabel label_5 = new JLabel("Partner Job");
-		label_5.setBounds(289, 96, 84, 15);
+		JLabel label_5 = new JLabel("مهنة الزوج");
+		label_5.setBounds(327, 96, 75, 15);
 		panel_1.add(label_5);
 
-		JLabel lblChildren = new JLabel("Children");
-		lblChildren.setBounds(298, 133, 67, 15);
+		JLabel lblChildren = new JLabel("عدد الأطفال");
+		lblChildren.setBounds(327, 133, 75, 15);
 		panel_1.add(lblChildren);
 
 		textField = new JTextField( );
 		textField.setColumns(10);
-		textField.setBounds(135, 12, 114, 19);
+		textField.setBounds(154, 12, 114, 19);
 		panel_1.add(textField);
 
 		textField_1 = new JTextField( );
 		textField_1.setColumns(10);
-		textField_1.setBounds(135, 43, 114, 19);
+		textField_1.setBounds(154, 43, 114, 19);
 		panel_1.add(textField_1);
 
 		textField_2 = new JTextField( );
 		textField_2.setColumns(10);
-		textField_2.setBounds(135, 74, 114, 19);
+		textField_2.setBounds(154, 74, 114, 19);
 		panel_1.add(textField_2);
 
 		textField_3 = new JTextField( );
@@ -157,28 +158,28 @@ public class MainWin {
 
 		textField_6 = new JTextField( );
 		textField_6.setColumns(10);
-		textField_6.setBounds(135, 105, 114, 19);
+		textField_6.setBounds(154, 105, 114, 19);
 		panel_1.add(textField_6);
 
-		JLabel label_7 = new JLabel("Address");
-		label_7.setBounds(25, 105, 84, 15);
+		JLabel label_7 = new JLabel("العنوان الشخصي");
+		label_7.setBounds(19, 105, 111, 15);
 		panel_1.add(label_7);
 
 		textField_7 = new JTextField( );
 		textField_7.setColumns(10);
-		textField_7.setBounds(135, 136, 114, 19);
+		textField_7.setBounds(154, 136, 114, 19);
 		panel_1.add(textField_7);
 
-		JLabel label_8 = new JLabel("Phone number");
-		label_8.setBounds(12, 135, 111, 15);
+		JLabel label_8 = new JLabel("الهاتف");
+		label_8.setBounds(47, 135, 54, 15);
 		panel_1.add(label_8);
 
-		JRadioButton radioButton = new JRadioButton("Yes");
-		radioButton.setSelected(true);
+		JRadioButton radioButton = new JRadioButton("نعم");
 		radioButton.setBounds(424, 18, 61, 19);
 		panel_1.add(radioButton);
 
-		JRadioButton radioButton_1 = new JRadioButton("No");
+		JRadioButton radioButton_1 = new JRadioButton("لا");
+		radioButton_1.setSelected(true);
 		radioButton_1.setBounds(490, 18, 61, 19);
 		panel_1.add(radioButton_1);
 
@@ -193,31 +194,31 @@ public class MainWin {
 		panel_1.add(spinner);
 
 		JPanel panel_2 = new JPanel( );
-		tabbedPane.addTab("Administrative Situtation", null, panel_2, null);
+		tabbedPane.addTab("الوضعية الإدارية", null, panel_2, null);
 		panel_2.setLayout(null);
 
-		JLabel lblCadre = new JLabel("Cadre");
-		lblCadre.setBounds(41, 15, 53, 15);
+		JLabel lblCadre = new JLabel("الإطار");
+		lblCadre.setBounds(46, 15, 53, 15);
 		panel_2.add(lblCadre);
 
-		JLabel lblReference_1 = new JLabel("Financial ref");
-		lblReference_1.setBounds(19, 45, 97, 15);
+		JLabel lblReference_1 = new JLabel("ر. المنصب المالي");
+		lblReference_1.setBounds(15, 45, 114, 15);
 		panel_2.add(lblReference_1);
 
-		JLabel lblRentReference = new JLabel("Rent ref");
-		lblRentReference.setBounds(25, 75, 84, 15);
+		JLabel lblRentReference = new JLabel("ر. التأجير");
+		lblRentReference.setBounds(36, 75, 72, 15);
 		panel_2.add(lblRentReference);
 
-		JLabel lblEmployment = new JLabel("Employment");
-		lblEmployment.setBounds(305, 22, 111, 15);
+		JLabel lblEmployment = new JLabel("تاريخ التوظيف");
+		lblEmployment.setBounds(303, 22, 92, 15);
 		panel_2.add(lblEmployment);
 
-		JLabel lblJoined = new JLabel("Joined");
-		lblJoined.setBounds(318, 59, 84, 15);
+		JLabel lblJoined = new JLabel("تاريخ الإلتحاق بالكلية");
+		lblJoined.setBounds(283, 59, 132, 15);
 		panel_2.add(lblJoined);
 
-		JLabel lblReason = new JLabel("Reason");
-		lblReason.setBounds(318, 96, 84, 15);
+		JLabel lblReason = new JLabel("سبب الإنتقال");
+		lblReason.setBounds(310, 96, 81, 15);
 		panel_2.add(lblReason);
 
 		textField_9 = new JTextField( );
@@ -245,8 +246,8 @@ public class MainWin {
 		textField_14.setBounds(422, 130, 114, 19);
 		panel_2.add(textField_14);
 
-		JLabel lblMission = new JLabel("Mission");
-		lblMission.setBounds(318, 133, 84, 15);
+		JLabel lblMission = new JLabel("المهمة");
+		lblMission.setBounds(323, 133, 53, 15);
 		panel_2.add(lblMission);
 
 		textField_15 = new JTextField( );
@@ -254,8 +255,8 @@ public class MainWin {
 		textField_15.setBounds(151, 106, 114, 19);
 		panel_2.add(textField_15);
 
-		JLabel lblPreviousJob = new JLabel("Previous job ");
-		lblPreviousJob.setBounds(12, 105, 111, 15);
+		JLabel lblPreviousJob = new JLabel("مقر العمل السابق ");
+		lblPreviousJob.setBounds(12, 105, 121, 15);
 		panel_2.add(lblPreviousJob);
 
 		JComboBox<Cadre> comboBox = new JComboBox<Cadre>( );
@@ -265,13 +266,13 @@ public class MainWin {
 
 		JComboBox<String> comboBox_1 = new JComboBox<String>( );
 		comboBox_1.setModel(new DefaultComboBoxModel<String>(new String[] {
-						"Transfer", "Exam"
+						"إنتقال", "مبارة"
 		}));
 		comboBox_1.setBounds(422, 89, 114, 24);
 		panel_2.add(comboBox_1);
 
-		JLabel lblCurrentJob = new JLabel("Current job");
-		lblCurrentJob.setBounds(12, 135, 111, 15);
+		JLabel lblCurrentJob = new JLabel("مقر العمل الحالي");
+		lblCurrentJob.setBounds(17, 135, 111, 15);
 		panel_2.add(lblCurrentJob);
 
 		textField_5 = new JTextField( );
@@ -284,16 +285,16 @@ public class MainWin {
 		frame.getContentPane( ).add(panel_3);
 		panel_3.setLayout(null);
 
-		JLabel lblMrRchidAnas = new JLabel("Mr. Rchid Anas, 23 years old");
-		lblMrRchidAnas.setBounds(290, 12, 269, 15);
+		JLabel lblMrRchidAnas = new JLabel("السيد أنس ارشيد، 23 سنة");
+		lblMrRchidAnas.setBounds(397, 12, 162, 15);
 		panel_3.add(lblMrRchidAnas);
 
-		JLabel lblSingle = new JLabel("Grade 9, 3 diplomes");
-		lblSingle.setBounds(290, 25, 162, 15);
+		JLabel lblSingle = new JLabel("السلم 9/الرتبة 4");
+		lblSingle.setBounds(455, 25, 104, 15);
 		panel_3.add(lblSingle);
 
-		JLabel lblDiplomas = new JLabel("Notes:");
-		lblDiplomas.setBounds(134, 66, 100, 15);
+		JLabel lblDiplomas = new JLabel("الملاحظات:");
+		lblDiplomas.setBounds(293, 39, 76, 15);
 		panel_3.add(lblDiplomas);
 
 		JPanel panel_4 = new JPanel( );
@@ -309,14 +310,14 @@ public class MainWin {
 		panel_4.add(lblNewLabel);
 
 		JTextPane textPane = new JTextPane( );
-		textPane.setBounds(205, 66, 354, 49);
+		textPane.setBounds(134, 66, 425, 49);
 		panel_3.add(textPane);
 
-		JLabel lblM = new JLabel("CIN: M550630");
-		lblM.setBounds(134, 12, 100, 15);
+		JLabel lblM = new JLabel("ب.ت.و.: M550630");
+		lblM.setBounds(134, 12, 123, 15);
 		panel_3.add(lblM);
 
-		JLabel lblRef = new JLabel("ref: 15462021");
+		JLabel lblRef = new JLabel("ر.ت: 15462021");
 		lblRef.setBounds(134, 25, 100, 15);
 		panel_3.add(lblRef);
 
@@ -326,20 +327,22 @@ public class MainWin {
 		frame.getContentPane( ).add(panel_5);
 
 		String columnNames[][] = {
-						{
-										"Diploma", "Second", "ThirdCol",
-										"FourthCol"
-						}, { }
+		    {	"الشهادات", "الميزة", "المؤسسة",
+			"تاريخ الحصول عليها"
+		    }, {
+			"السلم", "الرتبة", "الرقم الإستدلالي",
+			"التاريخ"
+		    }
 		};
 		DefaultTableModel dataModel0 = new DefaultTableModel( );
-		for (int col = 0; col < columnNames.length; col++) {
-			dataModel0.addColumn(columnNames[col]);
+		for (int col = 0; col < columnNames[0].length; col++) {
+			dataModel0.addColumn(columnNames[0][col]);
 		}
 		table = new JTable(dataModel0);
 		table.setBounds(0, 24, 571, 112);
 		panel_5.add(table);
 
-		JLabel label_18 = new JLabel("Diplomas");
+		JLabel label_18 = new JLabel("الشهادات");
 		label_18.setBounds(250, 0, 70, 15);
 		panel_5.add(label_18);
 
@@ -348,11 +351,15 @@ public class MainWin {
 		panel_6.setBounds(12, 508, 571, 136);
 		frame.getContentPane( ).add(panel_6);
 
-		table_1 = new JTable( );
+		DefaultTableModel dataModel1 = new DefaultTableModel( );
+		for (int col = 0; col < columnNames[1].length; col++) {
+			dataModel1.addColumn(columnNames[1][col]);
+		}
+		table_1 = new JTable(dataModel1);
 		table_1.setBounds(0, 24, 571, 112);
 		panel_6.add(table_1);
 
-		JLabel lblUplifts = new JLabel("Uplifts");
+		JLabel lblUplifts = new JLabel("الترقيات");
 		lblUplifts.setBounds(250, 0, 70, 15);
 		panel_6.add(lblUplifts);
 	}
