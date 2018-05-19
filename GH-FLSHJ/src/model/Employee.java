a0d39c54af31b145815386e80789c07ebbca74f3
Anas Rchid <0x0584@protonmail.com>
2018-05-19 20:44:07 +0000

    filtering in MainWin based on Type

--- GH-FLSHJ/src/model/Employee.java
+++ GH-FLSHJ/src/model/Employee.java
@@ -31,7 +31,22 @@ public class Employee extends Person {
 	}
 
 	public static enum Type {
-		Normal, Prof, All
+		Normal, Prof, All;
+		public static Type filter(boolean a, boolean b) {
+			Type type;
+			
+			if (a && b) {
+				type = Type.All;
+			} else if (a) {
+				type = Type.Prof;
+			} else if (b) {
+				type = Type.Normal;
+			} else {
+				type = Type.All;
+			}
+
+			return type;
+		}
 	}
 
 	protected Cadre cadre;
